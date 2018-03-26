ba4e29d0c8a039906bedd03c4198e6118051aa75
Tiago <tmcrs123@gmail.com>
2018-03-26 20:59:33 +0100

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
+import { ProductDetailComponentComponent } from './product-detail-component/product-detail-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductPageComponent
+    ProductPageComponent,
+    ProductDetailComponentComponent
   ],
   imports: [
     BrowserModule,
